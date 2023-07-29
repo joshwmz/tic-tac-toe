7c937f66d6112f367ba2080b9548e885ac86a7e0
Josh Wong <122000684+joshwmz@users.noreply.github.com>
2023-07-29 15:59:39 +1000

    Make game playable on webpage with JS
    Add displayControl module pattern to bridge HTML and JS, making
    interactive and playable game on the webpage. The formControl module
    pattern A checkRound function takes in name of the players and starts
    the game checks the grid after every player move on whether the game is
    over, either by a player winning or a tie game. A button appears once
    the game is over to invite players to play again.

--- javascript.js
+++ javascript.js
@@ -1,164 +1,212 @@
 const gameBoard = (() => {
-
     const grid = [[null, null, null], 
                   [null, null, null], 
                   [null, null, null]];
-    
     return {grid};
-
 })();
 
 
 
-const Player = (thisPlayer, playerNumber) => {
-
+const Player = (playerName, playerNumber) => {
     let marker = null;
-
     if (playerNumber == 1) {
         marker = 'x';
     } else if (playerNumber == 2) {
         marker = 'o';
-    } else {
-        console.log(`Too many players! ${thisPlayer} cannot enter`);
-        return;
     }
-
-    const name = thisPlayer;
+    const name = playerName;
     const index = playerNumber;
-
     return {name, index, marker};
-
 }
 
 
 
-const match = (() => {
+const game = (() => {
 
-    const playGame = (playerOne, playerTwo) => {
-        alert("Enter row and column index with a comma in between to successfully make your move! i.e. row 1, column 0 = \"1,0\"");
-        let playerTurn = 1; // Start with playerOne
-        for (let turns = 0; turns < 9; turns++) {
-            if (playerTurn == playerOne.index) {
-                playRound(playerOne);
-                playerTurn = 2;
-            }   else if (playerTurn == playerTwo.index) {
-                playRound(playerTwo);
-                playerTurn = 1;
-            }
-            let winner = checkForWinner();
-            if (winner !== undefined) {
-                return console.log(`${winner.name} won!`);
-            }
-            if (turns == 9) {
-                return console.log("Tie game, play again!");
-            }
+    let roundCount = 1;
+    let playerTurn = 1; // playerOne starts first
+    let winner = null;
+
+    const start = () => {
+        roundCount = 1;
+        playerTurn = 1;
+        winner = null;
+        let starter = checkTurn();
+        displayControl.start(starter);
+    }
+
+    const checkTurn = () => { // if it's player's turn, return the player
+        if (playerTurn == 1) {
+            return playerOne;
+        } else {
+            return playerTwo;
         }
     }
 
-    const playRound = (player) => {
-        let roundComplete = false;
-        do {
-            let move = prompt(`${player.name}: Make your move!`); // user input must be row index followed by column index, and comma separated
-            let rowIndex = Number(move.charAt(0));
-            let colIndex = Number(move.charAt(2));
-            if (move.length == 3 &&
-                typeof rowIndex === 'number' &&
-                typeof colIndex === 'number') {
-                if (gameBoard.grid[rowIndex][colIndex] == null) {
-                    gameBoard.grid[rowIndex][colIndex] = player;
-                    roundComplete = true;
-                } else {
-                    console.log('This cell\'s been occupied!');
-                }
-            } else {
-                console.log('Invalid input, try again!');
-            }
-        } while (roundComplete == false);
+    const nextTurn = (player) => { // change to next player once round is complete
+        if (player.index == 1) {
+            playerTurn = 2;
+        } else {
+            playerTurn = 1;
+        }
     }
 
-    const checkForWinner = () => {
+    const playRound = (gridIndex, rowIndex, colIndex) => {
+        let player = checkTurn();
+        if (gameBoard.grid[rowIndex][colIndex] == null) { // return true if round is successfully played
+            gameBoard.grid[rowIndex][colIndex] = player;          
+            displayControl.addMarker(gridIndex, player);
+            return checkRound(player);
+        } else { // return false if cell has been occupied
+            displayControl.blocker(player);
+        }
+    }
+
+    const checkRound = (player) => {
+        winner = checkForWin();
+        if (winner) {
+            displayControl.announceWinner(player);
+            return true;
+        } else if (roundCount == 9) {
+            displayControl.tieGame();
+            return true;
+        } else {
+            nextTurn(player);
+            displayControl.nextTurn(checkTurn());
+            roundCount++;
+        }
+    }
+
+    const checkForWin = () => {
         for (let gridLength = 0; gridLength < 3; gridLength++) {
-            // horizontal
+            // check horizontal
             if (gameBoard.grid[gridLength][0] !== null &&
                 (gameBoard.grid[gridLength][0] == gameBoard.grid[gridLength][1] &&
                  gameBoard.grid[gridLength][0] == gameBoard.grid[gridLength][2] &&
                  gameBoard.grid[gridLength][1] == gameBoard.grid[gridLength][2])) {
-                    return gameBoard.grid[gridLength][0];
+                    return true;
                 }
-            // vertical
+            // check vertical
             if (gameBoard.grid[0][gridLength] !== null && 
                 (gameBoard.grid[0][gridLength] == gameBoard.grid[1][gridLength] &&
                  gameBoard.grid[0][gridLength] == gameBoard.grid[2][gridLength] &&
                  gameBoard.grid[1][gridLength] == gameBoard.grid[2][gridLength])) {
-                    return gameBoard.grid[0][gridLength];
+                    return true;
             }
         }
-        // diagonal
+        // check diagonal
         if (gameBoard.grid[0][0] !== null && 
             (gameBoard.grid[0][0] == gameBoard.grid[1][1] &&
              gameBoard.grid[0][0] == gameBoard.grid[2][2] &&
              gameBoard.grid[1][1] == gameBoard.grid[2][2])) {
-                return gameBoard.grid[0][0];
+                return true;
         }
         if (gameBoard.grid[0][2] !== null &&
             (gameBoard.grid[0][2] == gameBoard.grid[1][1] &&
              gameBoard.grid[0][2] == gameBoard.grid[2][0] &&
              gameBoard.grid[1][1] == gameBoard.grid[2][0])) {
-                return gameBoard.grid[0][2];
+                return true;
             }
     }
 
-    return {playGame};
+    return {start, playRound};
 
 })();
 
 
-const inputOne = prompt("Enter player one name");
-const inputTwo = prompt("Enter player two name");
-const playerOne = Player(inputOne, 1);
-const playerTwo = Player(inputTwo, 2);
 
-console.log(playerOne, playerTwo);
-console.log(gameBoard.grid);
-match.playGame(playerOne, playerTwo);
+const displayControl = ((grid, instruction, playAgain) => {
+
+    let gameOver = false;
+
+    const playerInput = () => {
+        grid.addEventListener('click', (event) => {
+            if (!gameOver) {
+                let gridIndex = event.target.id; // get gridIndex
+                let rowIndex = gridIndex.charAt(5);
+                let colIndex = gridIndex.charAt(6);
+                gameOver = game.playRound(gridIndex, rowIndex, colIndex); // play round
+                if (gameOver) {
+                    playAgain.style.display = 'block';
+                }
+            }
+        });
+
+    }
+
+    const replay = () => {
+        playAgain.addEventListener('click', () => {
+            for (let r = 0; r < 3; r++) {
+                for (let c = 0; c < 3; c++) {
+                    gameBoard.grid[r][c] = null;
+                    document.getElementById(`grid-${r}${c}`).innerHTML = "";
+                }
+            }
+            playAgain.style.display = 'none';
+            gameOver = false;
+            game.start();
+        });
+    }
+
+    const start = (starter) => {
+        instruction.innerHTML = `${starter.name} starts first!`;
+    }
+    
+    const addMarker = (gridIndex, player) => {
+        document.getElementById(gridIndex).innerHTML = player.marker;
+    }
+
+    const nextTurn = (player) => {
+        instruction.innerHTML = `${player.name}'s turn!`;
+    }
+
+    const blocker = (player) => {
+        instruction.innerHTML = `This cell's occupied! It's still ${player.name}'s turn.`;
+    }
+
+    const announceWinner = (player) => {
+        instruction.innerHTML = `${player.name} won!`;
+    }
+
+    const tieGame = () => {
+        instruction.innerHTML = 'It\'s a tie!';
+    }
+
+    return {playerInput, replay, start, addMarker, nextTurn, blocker, announceWinner, tieGame};
+})(document.getElementById('grid-container'), 
+   document.getElementById('instruction'),
+   document.getElementById('play-again'));
 
 
 
+const formControl = ((form, formContainer) => {
 
+    const openForm = () => {
+        formContainer.style.display = 'grid';
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            if (form.elements['player-1'].value !== '') {
+                playerOne.name = form.elements['player-1'].value;
+            }
+            if (form.elements['player-2'].value !== '') {
+                playerTwo.name = form.elements['player-2'].value;
+            }
+            closeForm();
+            game.start();
+            displayControl.playerInput();
+            displayControl.replay();
+        });
+    }
 
+    const closeForm = () => {
+        formContainer.style.display = 'none';
+    }
 
-// const displayController =  ((display) => {
+    return {openForm};
 
-//     // Sequence of events once a player makes a move:
+})(document.getElementById('form'),
+   document.getElementById('form-container'));
 
-//     // Event listener for user input
-//     display.addEventListener('click', (event) => {
-//         console.log(event.target.id);
-//     })
-
-
-//     // Display player marker in HTML
-
-//     const writeToDOM = () => {
-
-//         let childNumber = 0;
-
-//         for (let r = 0; r < 3; r++) { // loop through grid row
-//             for (let c = 0; c < 3; c++) { // loop through grid column
-//                 if (grid[r][c] == null) { // check if grid cell has input
-//                     childNumber ++;
-//                 } else {
-//                     display.children[childNumber].innerHTML = grid[r][c];
-//                     console.log(grid[r][c]);
-//                     childNumber ++;
-//                 }
-//             }
-//         }
-
-//         console.log(gameBoard.grid);
-
-//     }
-
-//     return {writeToDOM};
-
-// })(document.getElementById('grid-container'));
+const playerOne = Player('Player 1', 1);
+const playerTwo = Player('Player 2', 2);
+formControl.openForm();
